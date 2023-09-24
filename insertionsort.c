7a857c3520e7a6d8ff1e3533deeec2542798727d
madihanazir <125150474+madihanazir@users.noreply.github.com>
2023-09-24 16:55:27 +0530

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -5,13 +5,13 @@ int i,j,x;
 for(i=1;i<n;i++){  //cuz n-1 passes //Iterate through the array, starting from the second element (index 1)
 j=i-1; //Initialize j to the index of the previous element
 x=A[i];  //Store the current element in x.
-	// While j is within the bounds of the array and the element at A[j] is greater than x,
+	
+while(j>-1 && A[j]>x){  // While j is within the bounds of the array and the element at A[j] is greater than x,
         // shift the elements to the right to make space for x.
-while(j>-1 && A[j]>x){ 
-	//
-	A[j+1]= A[j]; // Insert x into its correct position in the sorted part of the array.
+	
+	A[j+1]= A[j]; 
 	j--;
-} A[j+1]=x;	
+} A[j+1]=x;	// Insert x into its correct position in the sorted part of the array.
 
 }
 }
